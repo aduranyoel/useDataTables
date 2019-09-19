2d02f403102d20ba17528ba3250397f2d707a9e5
Tony Duran <yoet92@gmail.com>
2019-09-19 11:58:06 -0300

    useDataTable, (selection & className)

--- useDataTable.js
+++ useDataTable.js
@@ -1,10 +1,9 @@
-(function ($, $w) {
+(function ($) {
     'use strict';
     if ($ === undefined) throw new Error('NO SE ENCUENTRA "window.jQuery" PUEDE DESCARGARLO EN "https://jquery.com/"')
     $.fn.useDataTable = function (action, param, options) {
         if ($.fn.DataTable === undefined) throw new Error('NO SE ENCUENTRA "$.fn.DataTable". PUEDE DESCARGARLO EN "https://datatables.net/"')
         var thas = this[0];
-        $w.useDT_sourceMapping = new Object;
         action = action || '';
         param = param || '';
         options = options || {};
@@ -24,7 +23,7 @@
             "responsive": true,
             "colReorder": false,
             "fixedHeader": false,
-            "dom": "lfr<'usedatatable-header'>t<'usedatatable-footer'>ip",
+            "dom": "<'usedatatable-top'>lfr<'usedatatable-header'>t<'usedatatable-footer'>ip<'usedatatable-bottom'>",
             "selection": false,
             "bPaginate": false,
             "paging": false,
@@ -53,84 +52,18 @@
                 cursor: 'default',
                 callback: new Function
             },
-            filter: {
-                enabled: false,
-                targets: new Array
-            }
+            className: ' display'
         }, options.use ? options.use : {})
-        options.columnDefs = options.columnDefs || [];
-        var firstColumnDefs = [];
-        
-        
-        if (useOptions.filter.enabled === true){
-            $w.useDT_sourceMapping[thas.id] = $w.useDT_sourceMapping[thas.id] || new Object;
-            // tablaId: {
-            //     filtroLinea: {
-            //         name: 'Linea',
-            //         targetSelect: '',
-            //         values: [],
-            //         totalValues: [],
-            //         selectedValues: [],
-            //     }
-            // }
-            if (Array.isArray(useOptions.filter.targets)){useOptions.filter.targets = useOptions.filter.targets;}else{useOptions.filter.targets =  new Array;}
-            if (useOptions.filter.targets.length > 0){
-                var lenCols = options.columns.length;
-                var validTargets = new Array;
-                useOptions.filter.targets.forEach(function(target){
-                    if (target < lenCols){
-                        validTargets.push(target);
-                        options.columns[target].title += '<i data-idcol="'+target+'" data-source="'+options.columns[target].title+'" style="float: right;cursor:pointer;" class="fa fa-filter btn-usedatatable-filter"></i>';
-                    }
-                })
-                firstColumnDefs.push({"targets": validTargets, "orderable": false})
-            }
-        }
-        var customColumnDefs = firstColumnDefs.concat(options.columnDefs);
         var optionsResult = $.extend({}, options,{
             "createdRow": function( row, data, dataIndex ) {
                 $(row).attr('data-idrow', dataIndex);
                 options.createdRow = options.createdRow || new Function;
                 if (typeof options.createdRow === "function") options.createdRow.call(this, row, data, dataIndex);
-              },
-            "initComplete": function(settings, json){
-                if (useOptions.filter.enabled === true){
-                    if (Array.isArray(useOptions.filter.targets)){useOptions.filter.targets = useOptions.filter.targets;}else{useOptions.filter.targets =  new Array;}
-                    if (useOptions.filter.targets.length > 0){
-                        var lenCols = $(thas).DataTable().columns()[0].length;
-                        useOptions.filter.targets.forEach(function(target){
-                            if (target < lenCols){
-                                var source = $(thas).DataTable().columns(target).header()[0].textContent;
-                                $w.useDT_sourceMapping[thas.id][source] = $w.useDT_sourceMapping[thas.id][source] || new Object;
-                                $w.useDT_sourceMapping[thas.id][source].name = source;
-                                $w.useDT_sourceMapping[thas.id][source].targetSelect = '';
-                                $w.useDT_sourceMapping[thas.id][source].values = $(thas).DataTable().columns(target).data()[0];
-                                $w.useDT_sourceMapping[thas.id][source].totalValues = $(thas).DataTable().columns(target).data()[0];
-                                $w.useDT_sourceMapping[thas.id][source].selectedValues = [];
-                            }
-                        })
-                    }
-                    $('.btn-usedatatable-filter').on('click', function(e){
-                        e.preventDefault();
-                        var title = $(this).data('source') || "";
-                        var source = $(this).data('source') || "";
-                        var idCol = $(this).data('idcol') || "";
-    
-                        
-                        
-                        if (source) showModal(title, source);
-                    })
-                }
-
-                
-
-            options.initComplete = options.initComplete || new Function;
-            if (typeof options.initComplete === "function") options.initComplete.call(this, settings, json);
-            },
-            columnDefs: customColumnDefs
+              }
         })
         var settings = $.extend({}, defaults, optionsResult);
         function draw(settings) {
+            if (useOptions.className && typeof useOptions.className === "string") thas.className += useOptions.className;
             $(thas).DataTable(settings);
         }
         function redraw() {
@@ -196,7 +129,6 @@
         function clear(){
             $(thas).DataTable().clear().draw();
         }
-        
         if (useOptions.selection.enabled === true) {
             useOptions.selection.type = useOptions.selection.type || 'click';
             useOptions.selection.cursor = useOptions.selection.cursor || 'default';
@@ -207,214 +139,16 @@
                 if (typeof useOptions.selection.callback === "function") useOptions.selection.callback.call(this, row, data, index);
             });
             $(thas).on('mouseenter', 'tr', function () {
-                this.style.backgroundColor = 'rgb(246, 246, 246)';
                 this.style.cursor = useOptions.selection.cursor;
             });
             $(thas).on('mouseleave', 'tr', function () {
-                this.style.backgroundColor = '#fff';
                 this.style.cursor = 'default';
             });
         }
 
-        // Modal
-        var divModal = $w.document.createElement('div');
-        var divModalDialog = $w.document.createElement('div');
-        var divModalContent = $w.document.createElement('div');
-        var divModalHeader = $w.document.createElement('div');
-        var btnDismiss = $w.document.createElement('button');
-        var titleHeader = $w.document.createElement('h1');
-        var divModalBody = $w.document.createElement('div');
-        var divPanelBody = $w.document.createElement('div');
-        var divModalFilterContainer = $w.document.createElement('div');
-        var divModalFooter = $w.document.createElement('div');
-        var btnAceptar = $w.document.createElement('button');
-        btnAceptar.type = 'button';
-        btnAceptar.classList = 'btn btn-primary';
-        btnAceptar.setAttribute('data-dismiss', 'modal');
-        btnAceptar.innerHTML = '<span>ACEPTAR</span>';
-        divModalFooter.classList = 'modal-footer';
-        divModalFooter.appendChild(btnAceptar);
-        divModalFilterContainer.setAttribute('id', 'modal-filter-container');
-        divPanelBody.classList = 'panel-body';
-        divPanelBody.appendChild(divModalFilterContainer);
-        divModalBody.classList = 'modal-body';
-        divModalBody.appendChild(divPanelBody);
-        titleHeader.classList = 'modal-title';
-        btnDismiss.type = 'button';
-        btnDismiss.classList = 'close';
-        btnDismiss.setAttribute('data-dismiss', 'modal');
-        btnDismiss.setAttribute('aria-label', 'Close');
-        btnDismiss.innerHTML = '<span aria-hidden="true">X</span>';
-        divModalHeader.classList = 'modal-header';
-        divModalHeader.appendChild(titleHeader);
-        divModalHeader.appendChild(btnDismiss);
-        divModalContent.classList = 'modal-content';
-        divModalContent.appendChild(divModalHeader);
-        divModalContent.appendChild(divModalBody);
-        divModalContent.appendChild(divModalFooter);
-        divModalDialog.classList = 'modal-dialog';
-        divModalDialog.role = 'document';
-        divModalDialog.appendChild(divModalContent);
-        divModal.setAttribute('id', 'modal-filter');
-        divModal.classList = 'modal fade modal-md';
-        divModal.tabIndex = '-1';
-        divModal.role = 'dialog';
-        divModal.setAttribute('data-backdrop', 'static');
-        divModal.setAttribute('data-keyboard', 'false');
-        divModal.setAttribute('data-source', '');
-        divModal.setAttribute('data-title', '');
-        divModal.appendChild(divModalDialog);
-        
-        
-
-        function showModal(title,source) {
-            var modal = $(divModal);
-            $(titleHeader).text(title);
-            modal.data('source', source);
-            modal.data('title', title);
-            applyMenuFilter(divModalFilterContainer, $w.useDT_sourceMapping[thas.id][source].values, $w.useDT_sourceMapping[thas.id][source].selectedValues);
-            modal.modal('show');
-            //AdministrarDefectos.botonAceptarListener();
-        }
-        function applyMenuFilter(selector,values,selectedValues) {
-            function siSelect(selected, current){
-                if (selected.some(function(e){
-                    return e === current;
-                })){
-                    return 'checked';
-                }
-                return '';
-            }
-            var inputSearch = $w.document.createElement('input');
-            inputSearch.type = 'text';
-            inputSearch.onkeyup = search.bind(this);
-            inputSearch.placeholder = 'BUSCAR';
-            var ulContainer = $w.document.createElement('ul');
-            values.forEach(function(e, i){
-                var li = $w.document.createElement('li');
-                li.style.listStyle = 'none';
-                li.innerHTML = '<div class="checkbox checkbox-primary"><input '+siSelect(selectedValues, e)+' id="useDTcheckbox'+i+'" class="styled" type="checkbox"><label for="useDTcheckbox'+i+'">'+e+'</label></div>';
-                ulContainer.appendChild(li);
-            })
-            selector.innerHTML = '';
-            selector.appendChild(inputSearch);
-            selector.appendChild(ulContainer);
-
-        }
-        function botonAceptarListener() {
-            UtilJs.addCommonListener({
-                selector: '.btnAceptarModalFilter',
-                event: "click",
-                callback: function (evt) {
-    
-                    var modal = $(this).parents().find('#modal-filter');
-                    var source = modal.data('source');
-                    var title = modal.data('title');
-    
-                    var manualImput = AdministrarDefectos.sourceMapping[source].selectedValues.filter(function (item) {
-                        return item.manualInput !== undefined && item.manualInput;
-                    });
-                    AdministrarDefectos.sourceMapping[source].selectedValues = manualImput.concat(modal.find('#modal-filter-container').menuFilter('selectedValues'));
-    
-                    AdministrarDefectos.setCantidadValoresSeleccionados(source);
-                    // Extra
-                    //filtro para Linea
-                    if (source === "filtroPlanta") {
-                        if (AdministrarDefectos.sourceMapping.filtroProceso.selectedValues.length === 0 && AdministrarDefectos.sourceMapping.filtroTipoLinea.selectedValues.length === 0) {
-                            if (AdministrarDefectos.sourceMapping.filtroPlanta.selectedValues.length > 0) {
-                                AdministrarDefectos.sourceMapping.filtroLinea.values = AdministrarDefectos.sourceMapping.filtroLinea.totalValues.filter(function (e, i) {
-                                    if (AdministrarDefectos.sourceMapping.filtroPlanta.selectedValues.some(function (planta) {
-                                        return planta.codigo === e.planta;
-                                    })) { return true; }
-                                });
-                            }
-                        } else {
-                            if (AdministrarDefectos.sourceMapping.filtroPlanta.selectedValues.length > 0) {
-                                AdministrarDefectos.sourceMapping.filtroLinea.values = AdministrarDefectos.sourceMapping.filtroLinea.values.filter(function (e, i) {
-                                    if (AdministrarDefectos.sourceMapping.filtroPlanta.selectedValues.some(function (planta) {
-                                        return planta.codigo === e.planta;
-                                    })) { return true; }
-                                });
-                            }
-                        }
-                    }
-                    if (source === "filtroProceso") {
-                        if (AdministrarDefectos.sourceMapping.filtroPlanta.selectedValues.length === 0 && AdministrarDefectos.sourceMapping.filtroTipoLinea.selectedValues.length === 0) {
-                            if (AdministrarDefectos.sourceMapping.filtroProceso.selectedValues.length > 0) {
-                                AdministrarDefectos.sourceMapping.filtroLinea.values = AdministrarDefectos.sourceMapping.filtroLinea.totalValues.filter(function (e, i) {
-                                    if (AdministrarDefectos.sourceMapping.filtroProceso.selectedValues.some(function (proceso) {
-                                        return proceso.codigo === e.proceso;
-                                    })) { return true; }
-                                });
-                            }
-                        } else {
-                            if (AdministrarDefectos.sourceMapping.filtroProceso.selectedValues.length > 0) {
-                                AdministrarDefectos.sourceMapping.filtroLinea.values = AdministrarDefectos.sourceMapping.filtroLinea.values.filter(function (e, i) {
-                                    if (AdministrarDefectos.sourceMapping.filtroProceso.selectedValues.some(function (proceso) {
-                                        return proceso.codigo === e.proceso;
-                                    })) { return true; }
-                                });
-                            }
-                        }
-                    }
-                    if (source === "filtroTipoLinea") {
-                        if (AdministrarDefectos.sourceMapping.filtroPlanta.selectedValues.length === 0 && AdministrarDefectos.sourceMapping.filtroProceso.selectedValues.length === 0) {
-                            if (AdministrarDefectos.sourceMapping.filtroTipoLinea.selectedValues.length > 0) {
-                                AdministrarDefectos.sourceMapping.filtroLinea.values = AdministrarDefectos.sourceMapping.filtroLinea.totalValues.filter(function (e, i) {
-                                    if (AdministrarDefectos.sourceMapping.filtroTipoLinea.selectedValues.some(function (tipoLinea) {
-                                        return tipoLinea.codigo === e.tipoLinea;
-                                    })) { return true; }
-                                });
-                            }
-                        } else {
-                            if (AdministrarDefectos.sourceMapping.filtroTipoLinea.selectedValues.length > 0) {
-                                AdministrarDefectos.sourceMapping.filtroLinea.values = AdministrarDefectos.sourceMapping.filtroLinea.values.filter(function (e, i) {
-                                    if (AdministrarDefectos.sourceMapping.filtroTipoLinea.selectedValues.some(function (tipoLinea) {
-                                        return tipoLinea.codigo === e.tipoLinea;
-                                    })) { return true; }
-                                });
-                            }
-                        }
-                    }
-    
-                }
-            });
-        }
-        function setCantidadValoresSeleccionados(source) {
-            $("#" + source.replace("filtro", "cantidad")).text(AdministrarDefectos.sourceMapping[source].selectedValues.length ? "(" + AdministrarDefectos.sourceMapping[source].selectedValues.length + ")" : "");
-        }
-        // <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
-
-        // <ul id="myUL">
-        // <li><a href="#">Adele</a></li>
-        // <li><a href="#">Agnes</a></li>
-
-        // <li><a href="#">Billy</a></li>
-        // <li><a href="#">Bob</a></li>
-
-        // <li><a href="#">Calvin</a></li>
-        // <li><a href="#">Christina</a></li>
-        // <li><a href="#">Cindy</a></li>
-        // </ul>
-        function search(inputEle, ulEle) {
-            var input, filter, ul, li, a, i, txtValue;
-            input = document.getElementById("myInput");
-            filter = input.value.toUpperCase();
-            ul = document.getElementById("myUL");
-            li = ul.getElementsByTagName("li");
-            for (i = 0; i < li.length; i++) {
-                a = li[i].getElementsByTagName("a")[0];
-                txtValue = a.textContent || a.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    li[i].style.display = "";
-                } else {
-                    li[i].style.display = "none";
-                }
-            }
-        }
         function help(){
             return '\n'+
-            'Metodos:\n\n'+
+            'Acciones:\n\n'+
             'help     (): Muestra esta ayuda\n'+
             'draw     (settings?: object): Dibuja el DataTable (default)\n'+
             'data     (idRow?: string || number): Retorna datos de la(s) fila(s)\n'+
@@ -424,7 +158,7 @@
             'delRow   (idRow: string || number): Elimina la fila seleccionada\n'+
             'totalCol (idCol?: string || number): Retorna sumatoria de columna(s)\n'+
             'totalRow (idRow?: string || number): Retorna sumatoria de fila(s)\n\n'+
-            'Settings:\n'+
+            'Configuraciones Extra:\n'+
             '...\n'+
             'use: {\n'+
             '    ...                                     // Oppciones de useDataTable\n'+
@@ -436,8 +170,8 @@
             '    cursor: "default",                      // Cursor? (default: "default")\n'+
             '    callback: function(row, data, index){   // Accion del evento\n'+
             '\n'+
-            '    }\n'+
-            '}\n'
+            '    },\n'+
+            ' className: "table-responsive wrap etc"     // Clases? (default: "display")\n'
         }
 
         switch (action) {
@@ -463,4 +197,4 @@
                 return draw(settings);
         }
     };
-})(window.jQuery, window);
+})(window.jQuery);
